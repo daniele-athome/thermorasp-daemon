2e2b87c16af74f76de2a811f547c1f90fe69ffa0
Daniele Ricci <daniele@casaricci.it>
2018-01-20 22:59:52 +0100

    Store sensor readings for passive sensors every 10 seconds
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- thermostat/backend.py
+++ thermostat/backend.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
 """The backend process."""
 
+import logging
 import asyncio
+import datetime
 
 from .database import scoped_session
 from . import app
 from .models import Sensor
+from .models.sensors import store_reading
 from .sensors import get_sensor_handler
+
+
+# TEST loggers
+log = logging.getLogger("root")
 
 
 def get_passive_sensors():
@@ -15,20 +22,22 @@ def get_passive_sensors():
         return [dict(s) for s in session.execute(stmt)]
 
 
-def read_passive_sensors():
+def read_passive_sensors(sensor_type):
     pasv_sensors = get_passive_sensors()
     for sensor_info in pasv_sensors:
         handler = get_sensor_handler(sensor_info['protocol'], sensor_info['address'])
-        reading = handler.read('temperature')
-        print("{}: {}".format(sensor_info['id'], reading))
+        reading = handler.read(sensor_type)
+        log.debug("{}: {}".format(sensor_info['id'], reading))
+        # store reading in database
+        store_reading(sensor_info['id'], sensor_type, datetime.datetime.now(), reading['unit'], reading['value'])
 
 
 async def backend():
     while app.is_running:
         print("BACKEND RUNNING")
 
-        # read from passive sensors
-        read_passive_sensors()
+        # read from passive temperature sensors
+        read_passive_sensors('temperature')
 
         # TODO handle programs
 
--- thermostat/models/sensors.py
+++ thermostat/models/sensors.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
-"""Model for a sensor."""
+"""Models for sensors and sensor readings."""
 
 from sqlalchemy import (
-    Column, String, Integer
+    Column, String, Integer, DateTime
 )
 
 from . import Base
+from .. import app
+from ..database import scoped_session
 
 
 class Sensor(Base):
@@ -41,3 +43,32 @@ class Sensor(Base):
     def __repr__(self):
         """ Show sensor object info. """
         return '<Sensor: {}>'.format(self.id)
+
+
+class Reading(Base):
+    __tablename__ = 'sensor_readings'
+
+    # Sensor id, type and timestamp are the key
+    sensor_id = Column(String(255), primary_key=True)
+    sensor_type = Column(String(20), primary_key=True)
+    timestamp = Column(DateTime(), primary_key=True)
+
+    # Reading contents
+    unit = Column(String(20))
+    value = Column(String(255))
+
+    # Methods
+    def __repr__(self):
+        """ Show sensor object info. """
+        return '<Reading: {}@{}>'.format(self.id, self.timestamp)
+
+
+def store_reading(sensor_id, sensor_type, timestamp, unit, value):
+    with scoped_session(app.database) as session:
+        reading = Reading()
+        reading.sensor_id = sensor_id
+        reading.sensor_type = sensor_type
+        reading.timestamp = timestamp
+        reading.unit = unit
+        reading.value = value
+        session.add(reading)
