18adaa3038426449de2be50450f3ba307182a45a
Daniele Ricci <daniele@casaricci.it>
2018-01-20 21:23:14 +0100

    Disable workers (backend skeleton function)
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- thermostat/__init__.py
+++ thermostat/__init__.py
@@ -1,7 +1,22 @@
 # -*- coding: utf-8 -*-
 
+import asyncio
+
 from sanic import Sanic
 
+
 app = Sanic(__name__)
+
+
+async def backend():
+    while app.is_running:
+        await asyncio.sleep(5)
+        print("BACKEND RUNNING")
+
+
+@app.listener('before_server_start')
+async def init_backend(sanic, loop):
+    asyncio.ensure_future(backend(), loop=loop)
+
 
 from . import controllers
