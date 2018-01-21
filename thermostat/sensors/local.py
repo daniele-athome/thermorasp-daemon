0a0aa94b1df5da8563588bf19a36ab45d558886b
Daniele Ricci <daniele@casaricci.it>
2018-01-21 18:57:37 +0100

    Sensor readings from GPIO/W1ThermSensor
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- thermostat/sensors/local.py
+++ thermostat/sensors/local.py
@@ -3,6 +3,18 @@
 
 from random import randint
 
+import importlib.util
+try:
+    importlib.util.find_spec('RPi.GPIO')
+    import RPi.GPIO as GPIO
+except ImportError:
+    from fake_rpi.RPi import GPIO as GPIO
+    # will skip W1ThermSensor modprobe call
+    import os
+    os.environ['W1THERMSENSOR_NO_KERNEL_MODULE'] = '1'
+
+from w1thermsensor import W1ThermSensor
+
 from . import BaseSensorHandler
 from .. import errors
 
@@ -29,7 +41,21 @@ class GPIOSensorHandler(BaseSensorHandler):
     def __init__(self, address):
         BaseSensorHandler.__init__(self, address)
 
-    # TODO
+    def read(self, sensor_type):
+        if sensor_type == 'temperature':
+            if os.environ['W1THERMSENSOR_NO_KERNEL_MODULE'] == '1':
+                # random temperature :D
+                temp = 23
+            else:
+                sensor = W1ThermSensor()
+                temp = sensor.get_temperature(unit=W1ThermSensor.DEGREES_C)
+
+            return {
+                'value': temp,
+                'unit': 'celsius'
+            }
+        else:
+            raise errors.NotSupportedError('Only temperature is supported')
 
 
 schemes = {
