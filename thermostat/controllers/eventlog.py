956286d623945455fc67835a40faaa702fe1d5e4
Daniele Ricci <daniele@casaricci.it>
2018-02-06 23:31:55 +0100

    Event log next/prev
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- thermostat/controllers/eventlog.py
+++ thermostat/controllers/eventlog.py
@@ -23,8 +23,8 @@ def serialize_event(event: EventLog):
 
 
 # noinspection PyUnusedLocal
-@app.get('/eventlog')
-async def index(request: Request):
+@app.get('/eventlog/next')
+async def index_next(request: Request):
     """List all events."""
 
     # actually it's more of an end_id, but whatever :)
@@ -42,6 +42,33 @@ async def index(request: Request):
         query = session.query(EventLog)
         if start_id > 0:
             query = query.filter(EventLog.id < start_id)
+
+        events = query.order_by(desc(EventLog.id)) \
+            .limit(page_size) \
+            .all()
+
+        return json([serialize_event(e) for e in events])
+
+
+# noinspection PyUnusedLocal
+@app.get('/eventlog/prev')
+async def index_prev(request: Request):
+    """List all events."""
+
+    if 'start_id' in request.args:
+        start_id = int(request.args['start_id'][0])
+    else:
+        start_id = 0
+
+    if 'page_size' in request.args:
+        page_size = int(request.args['page_size'][0])
+    else:
+        page_size = 20
+
+    with scoped_session(app.database) as session:
+        query = session.query(EventLog)
+        if start_id > 0:
+            query = query.filter(EventLog.id <= (start_id + page_size))
 
         events = query.order_by(desc(EventLog.id)) \
             .limit(page_size) \
