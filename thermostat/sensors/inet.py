a431be9f6879a00db3aba3d3bcb0436ff81ef496
Daniele Ricci <daniele@casaricci.it>
2018-12-06 21:03:33 +0100

    Port HTTP sensor handler to new API
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- thermostat/sensors/inet.py
+++ thermostat/sensors/inet.py
@@ -1,34 +1,62 @@
 # -*- coding: utf-8 -*-
 """Protocols for remote sensors (mainly TCP/IP)."""
 
+import asyncio
 import requests
+import datetime
+import urllib.parse as urllib_parse
 
 from . import BaseSensorHandler
-from .. import errors
+from .. import app
+from ..models import eventlog
 
 
-# FIXME must be ported to the new API
 class HTTPSensorHandler(BaseSensorHandler):
     """A sensor handler that requests data to a HTTP endpoint."""
 
-    def __init__(self, sensor_id: str, address: str):
-        BaseSensorHandler.__init__(self, sensor_id, address)
+    protocol = 'HTTP'
+    DEFAULT_INTERVAL = 60
 
-    def read(self, sensor_type):
-        if sensor_type == 'temperature':
+    def __init__(self, sensor_id: str, address: str, sensor_type: str):
+        BaseSensorHandler.__init__(self, sensor_id, address, sensor_type)
+        params = urllib_parse.parse_qs(address)
+        self.last_temperature = None
+        if params and 'interval' in params:
+            self.interval = int(params['interval'][0])
+        else:
+            self.interval = self.DEFAULT_INTERVAL
+
+    async def connected(self):
+        await self.timeout()
+        self.start_timer(self.interval)
+
+    async def timeout(self):
+        if self.type == 'temperature':
+            try:
+                temp, unit = await asyncio.get_event_loop().run_in_executor(None, self._read)
+            except ValueError:
+                app.eventlog.event_exc(eventlog.LEVEL_WARNING, self.id, 'exception')
+                return
+
+            if self.last_temperature is None or self.last_temperature != temp:
+                self.last_temperature = temp
+                await self.publish({
+                    'value': self.last_temperature,
+                    'unit': unit,
+                    'timestamp': datetime.datetime.now().isoformat(),
+                }, '/temperature', retain=True)
+
+    def _read(self):
+        try:
             r = requests.get(self.address)
             data = r.json()
-            try:
-                return {
-                    'value': float(data['value']),
-                    'unit': data['unit']
-                }
-            except KeyError:
-                raise ValueError('Invalid sensor data')
-        else:
-            raise errors.NotSupportedError('Only temperature is supported')
+            return float(data['value']), data['unit']
+        except requests.RequestException as e:
+            raise ValueError('Connection error') from e
+        except KeyError as e:
+            raise ValueError('Invalid sensor data') from e
 
 
 schemes = {
-    'HTTP': HTTPSensorHandler,
+    HTTPSensorHandler.protocol: HTTPSensorHandler,
 }
